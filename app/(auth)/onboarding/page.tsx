b1ba392911bf1d4f8765d33ce9dc5c7f905ae64f
melwin-tedd <musictrendiez@gmail.com>
2023-08-13 17:19:17 +0400

    IDK

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -12,7 +12,7 @@ async function Page() {
 
   const userData = {
     id: user?.id,
-    ObjectId: userInfo?._id,
+    objectId: userInfo?._id,
     username: userInfo?.username || user?.username,
     name: userInfo?.name || user?.firstName || '',
     bio: userInfo?.bio || '',
